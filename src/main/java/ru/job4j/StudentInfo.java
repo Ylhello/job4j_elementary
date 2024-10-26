0ea043588d7433d6ee16514f25c31eeb12fb03c9
Yulya <ylhappy999@gmail.com>
2024-10-27 05:05:30 +1000

    3.3 Загрузка задания "StudentInfo" на Github [243563]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Yulia");
+        System.out.println("27.10.2024");
+    }
+}

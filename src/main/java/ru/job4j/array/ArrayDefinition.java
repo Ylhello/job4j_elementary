8af76207b12446b9cf23b9459b96ab734a0695eb
Yulya <ylhappy999@gmail.com>
2024-11-04 02:46:25 +1000

    5.2 Import задания "Размер массива" на Github [243582]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -1,14 +1,12 @@
 package ru.job4j.array;
 
-import java.util.Arrays;
-
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
-        System.out.println(Arrays.toString(ages));
-        System.out.println(Arrays.toString(surnames));
-        System.out.println(Arrays.toString(prices));
+        System.out.println("Размер массива равен: " + ages.length);
+        System.out.println("Размер массива равен: " + surnames.length);
+        System.out.println("Размер массива равен: " + prices.length);
     }
 }

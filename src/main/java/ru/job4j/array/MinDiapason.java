69c21f917df7462dc98ed661b425cd36bfaace26
Yulya <ylhappy999@gmail.com>
2024-11-23 01:00:49 +1000

    6.8 Import задания "Поиск минимума в диапазоне" на Github [505239]

---
+++ src/main/java/ru/job4j/array/MinDiapason.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+public class MinDiapason {
+    public static int findMin(int[] array, int start, int finish) {
+        int min = array[start];
+        for (int i = 0; i < finish; i++) {
+            if (array[i] > min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+}
+
+
+
+
